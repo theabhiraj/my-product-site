2dccadff8be36bfcbac983d9c9aaaf6b7e3d00a6
theabhiraj <theabhiraj.in@gmail.com>
2025-03-30 15:52:22 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Admin from "./pages/Admin";
 import Login from "./pages/Login";
